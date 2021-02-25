30fcede80930e335b3d3450f9a5c75d388de3a6b
Vincent550102 <5020559@gmail.com>
2021-02-25 22:56:54 +0800

    daily upd

---
+++ picoCTF/Cryptography/la-cifra-de/solve.py
@@ -0,0 +1,5 @@
+s = "pohzCZK{m311a50_0x_a1rn3x3_h1ah3x6kp60egf}"
+
+ans = ""
+for c in s:
+    if 
---
+++ picoCTF/Forensics/Pitter,Patter,Platters/flag.py
@@ -0,0 +1,3 @@
+f = "}.d.c.7.0.7.9.d.d._.3.<._.|.L.m._.1.1.1.t.5._.3.b.{.F.T.C.o.c.i.p"
+flag = "".join(_ for _ in f if _ !=".")
+print(flag[::-1])
