b5da751b225d265ae4b15114fa63103244a04d49
Vincent550102 <5020559@gmail.com>
2021-02-27 23:59:54 +0800

    d

---
+++ picoCTF/Forensics/WhitePages/solve.py
@@ -0,0 +1,10 @@
+# coding=utf8
+s = "                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                "
+typ = " "
+sol = ""
+for _ in s:
+    if _  == typ:
+        sol += '0'
+    else:
+        sol += '1'
+print(sol)
