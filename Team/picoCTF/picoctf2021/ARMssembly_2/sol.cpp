f6f8d3e94bbbbfdb5463a692f4d51addfe4c905c
Vincent550102 <5020559@gmail.com>
2021-03-22 21:55:35 +0800

    upd

---
+++ Team/picoCTF/picoctf2021/ARMssembly_2/sol.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long ans = 0;
+    long long tar = 1748687564;
+    for(long long i = 0; i<tar; i++){
+        ans+=3;
+    }
+    cout << ans << endl;
+}
+
