6dc5278f4baaec47783b178b269eabe7081c0eca
Vincent550102 <5020559@gmail.com>
2021-04-12 01:33:21 +0800

    upd

---
+++ Team/angstromctf/angstromctf2021/Jailbreak/extract.py
@@ -0,0 +1,28 @@
+from pwn import *
+import os, sys, stat
+
+
+context.arch = 'amd64'
+before_call_address = 0x10db
+after_call_address = 0x10ed
+
+for i in range(29):
+        elf = ELF('./jailbreak')
+        elf.asm(before_call_address,
+		f"""
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		mov rdi, {i}
+		""")
+        elf.asm(after_call_address, f'ret')
+        elf.save(f'patched{i}')
+        os.chmod(f'patched{i}', stat.S_IEXEC)
+        p = process(f'patched{i}')
+        print(f'{i}: ', p.recv())
+        p.close()
+        if i!=1:
+            os.unlink(f'patched{i}')
